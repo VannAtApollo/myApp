9346588135411f682e07987ffe4b0543412b695f
Vann Orton <vann@apollographql.com>
2023-03-01 23:48:41 -0600

    MyApp working

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { DetailsScreen } from './components/DetailsScreen';
+import { HomeScreen } from './components/HomeScreen';
+import { CameraScreen } from './components/CameraScreen';
 
-export default function App() {
+
+
+function StackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'My home' }}
+      />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={(HomeScreen)} options={{ title: 'My home' }} />
+        <Stack.Screen name="Details" component={(DetailsScreen)} />
+        <Stack.Screen name="Camera" component={(CameraScreen)} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ components/Header.js
@@ -0,0 +1,6 @@
+import { Button, View, Text } from 'react-native';
+
+
+export function Header() {
+    return;
+}
